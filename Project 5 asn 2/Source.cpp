2c4bc72ec1e7d7b3f834709ffeb251969f8edee8
Fazeel Hussain <Fhussain2644@conestogac.on.ca>
2022-09-22 23:29:41 -0400

    DEBUG functionality complete

--- Project 5 asn 2/Source.cpp
+++ Project 5 asn 2/Source.cpp
@@ -10,6 +10,7 @@ using namespace std;
 //create a structure to hold student data
 
 
+
 struct STUDENT_DATA {
 	string fname;
 	string lname;
@@ -17,18 +18,9 @@ struct STUDENT_DATA {
 
 
 int main() {
-	// create a vector to store the structs
-	//std::vector <STUDENT_DATA> students;
-
-	////u can add something to a vector using the .pushback, this will add it to the first available index of the vector
-	//STUDENT_DATA student1;
-	//students.push_back(student1);
-
-	////u can use .pop_back to remove the last ellement in the vector
-	//students.pop_back();
+	
 
 
-	//file i/o starts here
 	std::vector <STUDENT_DATA> students;
 	STUDENT_DATA tmpStudent;
 	string line;
@@ -41,16 +33,24 @@ int main() {
 
 			getline(ss, tmpStudent.lname, ',');
 			getline(ss, tmpStudent.fname, '\n');
-			//cout << tmpStudent.lname << "," << tmpStudent.fname << endl; //this will print out the list, but not from the vector
 			students.push_back(tmpStudent);
 		}
-		//cout << students.at(0).fname << students.at(0).lname; //prints out an item from the vector
 		stData.close();
 	}
 	else {
 		cout << "file did not open";
 	}
-	
+
+
+#ifdef _DEBUG
+	// print out the list of students
+	cout << "DEBUG Running \n \n";
+
+	for (int i = 0; i < students.size(); i++) {
+		cout << students.at(i).lname << "," << students.at(i).fname << endl;
+	}
+#endif // DEBUG
+
 	
 	return 1;
 }
