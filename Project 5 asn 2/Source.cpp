be51c061988b7f6937f7a8fb8d4b4180ab23bffd
Fazeel Hussain <Fhussain2644@conestogac.on.ca>
2022-09-23 00:42:50 -0400

    step 5 part 3 complete

--- Project 5 asn 2/Source.cpp
+++ Project 5 asn 2/Source.cpp
@@ -9,17 +9,45 @@ using namespace std;
 
 //create a structure to hold student data
 
+#define PreRelease
 
 
 struct STUDENT_DATA {
 	string fname;
 	string lname;
+	string email;
 };
 
 
 int main() {
 	
+#ifdef PreRelease
+	cout << "PreRelease is Running" << endl;
 
+	std::vector <STUDENT_DATA> students;
+	STUDENT_DATA tmpStudent;
+	string line;
+	std::ifstream stData("StudentData_Emails.txt");
+
+	if (stData.is_open()) {
+
+		while (getline(stData, line)) {
+			stringstream ss(line);
+
+			getline(ss, tmpStudent.lname, ',');
+			getline(ss, tmpStudent.fname, ',');
+			getline(ss, tmpStudent.email, '\n');
+			students.push_back(tmpStudent);
+		}
+		stData.close();
+	}
+	else {
+		cout << "file did not open";
+	}
+
+	
+#else 
+	cout << "Standard Version running \n";
 
 	std::vector <STUDENT_DATA> students;
 	STUDENT_DATA tmpStudent;
@@ -27,8 +55,8 @@ int main() {
 	std::ifstream stData("StudentData.txt");
 
 	if (stData.is_open()) {
-		
-		while ( getline(stData, line) ) {
+
+		while (getline(stData, line)) {
 			stringstream ss(line);
 
 			getline(ss, tmpStudent.lname, ',');
@@ -40,17 +68,27 @@ int main() {
 	else {
 		cout << "file did not open";
 	}
+#endif // PreRelease
 
 
 #ifdef _DEBUG
 	// print out the list of students
 	cout << "DEBUG Running \n \n";
 
+#ifdef PreRelease
+	for (int i = 0; i < students.size(); i++) {
+		cout << students.at(i).lname << "," << students.at(i).fname << "," << students.at(i).email << endl;
+	}
+#else
 	for (int i = 0; i < students.size(); i++) {
 		cout << students.at(i).lname << "," << students.at(i).fname << endl;
-	}
-#endif // DEBUG
+}
+
+#endif // PreRelease
 
 	
+#endif // DEBUG
+
+
 	return 1;
 }
